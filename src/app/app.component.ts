b5647b5ba9ca22c393dd97a0b2813da5533308fe
Carlos Rangel <carlos.rangel@us.sogeti.com>
2020-03-25 18:10:47 -0500

    Worked on side navigation styling. Brought it up-to-speed, will require optimization efforts later. Side navs now have hover and active style specs. Side navigation position set to fixed; Using ViewEncapsulation.None

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 import { MatDrawer } from '@angular/material';
 import { Overview } from './core/models/overview.model';
@@ -8,12 +8,49 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   title = '';
   version = '';
   overview: Overview;
+  folders: any[] = [
+    {
+      icon: 'videogame_asset',
+      name: 'Marvel Games',
+      // updated: 'Avenger Feud | Trivia',
+    },
+    {
+      icon: 'explore',
+      name: 'Explore',
+      // updated: 'Characters | Comics',
+    },
+    {
+      icon: 'map',
+      name: 'World Map',
+    }
+  ];
+  ops: any[] = [
+    {
+      icon: 'show_chart',
+      name: 'Trending Marvel',
+    },
+    {
+      icon: 'language',
+      name: 'Change Language',
+    }
+  ];
+  notes: any[] = [
+    {
+      icon: 'feedback',
+      name: 'Feedback',
+    },
+    {
+      icon: 'help',
+      name: 'Angular Initiative',
+    }
+  ];
   @ViewChild(MatDrawer, { static: false }) drawer: MatDrawer;
 
   constructor(private _sharedService: SharedService) {
